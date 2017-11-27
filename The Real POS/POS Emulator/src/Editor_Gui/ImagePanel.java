1924292873fa74e03136a33c01c972e478e1ef9c
flamnick <flamnick@gmail.com>
2017-11-27 17:42:32 -0500

    seperated projects

---
+++ The Real POS/POS Emulator/src/Editor_Gui/ImagePanel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Editor_Gui;
+
+/**
+ * Required to display images in dialog box. DO NOT DELETE.
+ * @author Team 8
+ */
+class ImagePanel {
+
+}
